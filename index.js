a6b18351356dfa98fddb6ef8ccfb43e43fb9a59c
Avi-24505 <arnavagarwal245@gmail.com>
2024-10-08 20:36:52 +0530

    closed the book, added rev btn funtionality, profile btn fnctnl

--- index.js
+++ index.js
@@ -22,17 +22,44 @@ pageTurnBtn.forEach((el, index) => {
 
 // contact me button when click
 const pages = document.querySelectorAll('.book-page.page-right');
-const contactMeBtn = document.querySelectorAll('.btn.contact-me');
+const contactMeBtn = document.querySelector('.btn.contact-me');
 
 contactMeBtn.onclick = () => {
     pages.forEach((page, index) => {
-        setTimeout(()=>{
+        setTimeout(() => {
             page.classList.add('turn');
 
-            setTimeout(()=> {
+            setTimeout(() => {
                 page.style.zIndex = 20+index;
             }, 500)
         }, (index +1)*200 +100)
     })
 }
 
+// create reverse index function
+let totalPages = pages.length;
+let pageNumber = 0;
+
+function reverseIndex(){
+    pageNumber--;
+    if(pageNumber<0){
+        pageNumber = totalPages-1;
+    }
+}
+
+// back profile button when click
+const backProfileBtn = document.querySelector('.back-profile');
+backProfileBtn.onclick = () => {
+    pages.forEach((_, index) => {
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNumber].classList.remove('turn');
+            setTimeout(() => {
+                reverseIndex();
+                pages[pageNumber].style.zIndex= 10 + index;
+            }, 500)
+        }, (index+1)*200+100)
+    })
+}
+
+// opening Animation
